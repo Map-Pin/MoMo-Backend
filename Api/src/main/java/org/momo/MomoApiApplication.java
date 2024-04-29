fdbd0760aa76656ce90f0fb46510b40b60b7ba33
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-04-29 16:10:08 +0900

    Init: Apply Multi Module

---
+++ Api/src/main/java/org/momo/MomoApiApplication.java
@@ -0,0 +1,11 @@
+package org.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MomoApiApplication.class,args);
+    }
+}
---
+++ Search/src/main/java/org/momo/MomoSearchApplication.java
@@ -0,0 +1,7 @@
+package org.momo;
+
+public class MomoSearchApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
