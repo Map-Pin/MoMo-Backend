527d5010872f6a6da3ca7bf7a4f3bb73229a9b55
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-05-25 15:01:28 +0900

    fix: Deleted Wrong Status and Deleted MemberHandler.java

--- Common/src/main/java/org/momo/common/status/SuccessStatus.java
+++ Common/src/main/java/org/momo/common/status/SuccessStatus.java
@@ -9,14 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum SuccessStatus {
     _OK(HttpStatus.OK,200,"성공입니다"),
 
-
     // UserController
-    MODEL_JOIN_SUCCESS(HttpStatus.OK, 200, "회원가입이 완료되었습니다"),
-    ARTIST_JOIN_SUCCESS(HttpStatus.OK, 200, "회원가입이 완료되었습니다"),
-    ARTIST_EXTRA_JOIN_SUCCESS(HttpStatus.OK, 200, "추가 회원 정보 기입이 완료되었습니다."),
+    USER_JOIN_SUCCESS(HttpStatus.OK, 200, "회원가입이 완료되었습니다"),
     USER_EXISTS(HttpStatus.OK, 200, "사용자 정보가 확인되었습니다."),
     NICKNAME_NOT_EXISTS(HttpStatus.OK, 200, "사용 가능한 닉네임입니다."),
-
 
     // AuthController
     LOGIN_SUCCESS(HttpStatus.OK,200,"토큰 검증에 성공하였습니다."),
