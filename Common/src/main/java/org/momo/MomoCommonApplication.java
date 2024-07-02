93bc1254714b7c683401e520ad737381aee3d00a
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-07-02 18:09:12 +0900

    :sparkles: 코드 정리

--- Common/src/main/java/org/momo/MomoCommonApplication.java
+++ Common/src/main/java/org/momo/MomoCommonApplication.java
@@ -2,14 +2,10 @@ package org.momo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class MomoDomainApplication {
+public class MomoCommonApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MomoDomainApplication.class, args);
+        SpringApplication.run(MomoCommonApplication.class,args);
     }
 }
-
-
